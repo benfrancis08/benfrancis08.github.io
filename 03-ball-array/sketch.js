b8b1292da910f2c22417acd8c60b4f5e5d0b6144
Ben Francis <ben3dprints1@gmail.com>
2026-03-04 15:25:28 -0600

    Added to ball array

--- 03-ball-array/sketch.js
+++ 03-ball-array/sketch.js
@@ -8,28 +8,46 @@ function setup() {
 
 function draw() {
   background(0);
+  spawnBall(mouseX, mouseY);
 
   for (let ball of ballArray) {
     // Move
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    if (ball.x - ball.radius > width + ball.radius) {
+      ball.x = -ball.radius;
+    }
+    if (ball.x < -ball.radius) {
+      ball.x = width + ball.radius;
+    }
+    if (ball.y - ball.radius > height + ball.radius) {
+      ball.y = -ball.radius;
+    }
+    if (ball.y < -ball.radius) {
+      ball.y = height + ball.radius;
+    }
+
     // Display
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius*2);
   }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let theBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 40),
+    r: random(255),
+    g: random(255),
+    b: random(255),
   };
   ballArray.push(theBall);
 }
